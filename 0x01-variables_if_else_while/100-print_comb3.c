8a66eb9bcd8ddc85bc5645f960ef11461e282133
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-12 01:15:50 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 48;
+	
+	while (n <= 56)
+	{
+		int m = 49;
+		while (m <= 57)
+		{
+			if (n < m)
+			{
+				putchar(n);
+				putchar (m);
+				if (n != 56) || (m != 57)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+			m++;
+		}
+		n++;
+	}
+	return (0);
+}
