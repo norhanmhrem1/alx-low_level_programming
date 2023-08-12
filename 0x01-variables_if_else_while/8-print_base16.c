54e4716c7829d2db4ae6a5806c651f3d1f524284
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-11 22:48:48 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = o ; i <= g ; i++)
+	{
+		putchar (i);
+	}
+	putchar('\n');
+	return (0);
+}
