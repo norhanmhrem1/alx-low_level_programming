6fd620e67f433719bdd0f90d4ace4ddaaa237898
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-11 22:23:11 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char ch = 'z';
+	
+	while (ch >= 'z')
+	{
+		putchar(ch);
+		ch = ch - 1;
+	}
+	putchar('\n');
+	return (0);
+}
