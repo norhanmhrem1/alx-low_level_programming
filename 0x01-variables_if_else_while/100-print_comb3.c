a281bdd13aac3eced39541889a773a1e19f4ddfb
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-12 01:19:28 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,18 @@
 int main(void)
 {
 	int n = 48;
-	
+
 	while (n <= 56)
 	{
 		int m = 49;
+
 		while (m <= 57)
 		{
 			if (n < m)
 			{
 				putchar(n);
-				putchar (m);
-				if (n != 56) || (m != 57)
+				putchar(m);
+				if (n != 56 || m != 57)
 				{
 					putchar (',');
 					putchar (' ');
