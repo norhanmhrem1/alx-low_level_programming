b13190f420183095a45d1e9d2b0a6bd87fe8ea29
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-11 21:23:19 -0700

    task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,12 @@
 
 int main(void)
 {
-	int i = 0; 
+	int i = 0;
+
 	for (i < 10; i++)
 	{
-		printf("%d", i); 
+		printf("%d", i);
 	}
-	putchar('\n'); 
-	return (0); 
+	putchar('\n');
+	return (0);
 }
