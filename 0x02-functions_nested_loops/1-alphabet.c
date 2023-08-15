1ac62946c5a3fcfbac92a07f0058e8f2a2d806ed
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-15 13:31:38 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints in lowercase,
+ * followed by a new line
+ */
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= z ; ch++)
+		_putchar(ch);
+	_putchar ('\n);
+}
