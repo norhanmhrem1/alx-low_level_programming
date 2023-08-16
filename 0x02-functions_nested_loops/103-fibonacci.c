c1106072065e881fe84832d496c2d331dfa1f3fe
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-15 22:50:54 -0700

    task15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	float totalsum;
+	unsigned long fib1 = 0, fib2 = 1, sum = 0;
+	float totalsum = 0;
 
 	while (1)
 	{
 		sum = fib1 + fib2;
 		if (sum > 4000000)
 			break;
-		if (sum < 4000000 && (sum % 2) == 0)
+		if (sum % 2 == 0)
 			totalsum = totalsum + sum;
 		fib1 = fib2;
 		fib2 = sum;
