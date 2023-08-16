ed5d7a8d5c64c4d9bf32537805271c550aab0f91
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-15 19:05:56 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	for (num = 0; num >= 9; num++)
 	{
 		_putchar(48);
-		for (multi = 0; multi <= 9; multi++)
+		for (multi = 1; multi <= 9; multi++)
 		{
 			_putchar(',');
 			_putchar(' ');
