55c0641ae4e2e5fe319fbeb9860dc0bba3710b78
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-17 14:43:11 -0700

    task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,17 @@
 
 void more_numbers(void)
 {
-	int row, mum;
+	int row, num;
 
 	for (row = 1; row <= 10; row++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 10)
-			{
-				_putchar(1 + 48);
-				_putchar(num % 10);
-			}
-			 _putchar(num + 48);
+				_putchar('1');
+			_putchar (num % 10 + '0');
 		}
 		_putchar('\n');
 	}
 }
+
