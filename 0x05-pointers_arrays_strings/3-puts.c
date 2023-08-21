78d03bd7441377df5d072c336de9c49ddc030349
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-21 13:22:13 -0700

    task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,16 @@
 /**
  *_puts - prints a string, followed by a new line, to stdout
  *@str: string to print
- */
+*/
 void _puts(char *str)
 {
-	int i;
+		int i;
 
-	for (i = 0; *str != '\0'; str++)
-	{
-		_putchar(*str + 0);
-		i++
-	}
+		for (i = 0; *str != '\0'; str++)
+		{
+			_putchar(*str + 0);
+			i++
+		}
 
 	_putchar('\n');
 }
