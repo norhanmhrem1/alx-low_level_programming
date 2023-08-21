f690e8ede060cf930f4c38a352f1641f8ef7dbdb
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-21 16:52:34 -0700

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *puts_half - a function that prints half of a string
+ *if odd len, n = (length_of_the_string - 1) / 2
+ *@str: input
+ *Return: half of input
+*/
+void puts_half(char *str)
+{
+	int size;
+
+	for (size = 0; str[size] != '\n'; size++)
+
+	size++;
+	for (size /= 2; str[size] != '\0'; size++)
+	{
+		_putchar(str[size]);
+	}
+}
