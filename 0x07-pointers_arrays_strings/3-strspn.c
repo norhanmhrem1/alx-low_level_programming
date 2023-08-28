f6b49943d0442306a71159ffd64039d80982f2a9
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-28 16:38:25 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *_strspn: gets the length of a prefix substring.
- *@s: srting
+ *_strspn - gets the length of a prefix substring.
+ *@s: srting.
  *@accept: bytes.
- *return: unsigned int.
+ *
+ *return:  the number of bytes in the initial segment of s .
 */
 
 unsigned int _strspn(char *s, char *accept)
