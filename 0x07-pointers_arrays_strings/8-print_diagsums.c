88a08f8b7e7615f8708e487815f5f3bf3808d04b
noormhrem1 <norhan_mehrem@yahoo.com>
2023-08-28 17:48:11 -0700

    task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0 ; i < size ; i++)
 	{
 		sum1 = sum1 + a[i];
-		sum2 = a[size - i - 1];
+		sum2 = sum2 + a[size - i - 1];
 		a = a + size;
 	}
 	printf("%d, ", sum1)
