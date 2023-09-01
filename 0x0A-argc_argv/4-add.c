e15174001e75d4ecdc83f1eb4d76adec52b74b04
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-01 08:47:35 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		{
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
-			sum = sum + atoi(argv[argc]);
+		sum = sum + atoi(argv[argc]);
 		}
 	}
 	printf("%d\n", sum);
