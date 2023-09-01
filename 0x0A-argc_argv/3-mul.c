72835898a0e552059b87bdc81e5b63b2d7573ba5
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-01 05:28:58 -0700

    task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
  *main - multiplies two numbers
@@ -11,12 +11,14 @@
 
 int main(int argc, char **argv)
 {
-	int multi;
+	int result, num1, num2;
 
 	if (argc == 3)
 	{
-		multi = _atoi(argv[1]) * _atoi(argv[2]);
-		printf("%d\n", multi);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
 	}
 	else
 	{
