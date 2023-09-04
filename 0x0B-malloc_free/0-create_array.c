93dc8c75e9f3aef58de61a85010afa1865060ee8
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-04 09:23:10 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	int i;
 
 	ptr = malloc((sizeof(char) * size));
-			if (size == 0 || ptr == null)
+			if (size == 0 || ptr == NULL)
 				return (0);
 
 			for (i = 0; i < size; i++)
