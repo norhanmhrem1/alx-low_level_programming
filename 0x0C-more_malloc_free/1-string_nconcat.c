8f82624586ef51583ff49b0ce6858a632c7a2152
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-07 10:26:19 -0700

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < size1; i++)
 	{
 		ptr[i] = s1[i];
 	}
