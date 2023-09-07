83865deae85c963d20e1f8633627eea8194dec5a
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-07 13:34:22 -0700

    task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,14 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
+	char *ptr = s;
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-	return (s);
+	return (ptr);
 }
 /**
  *_calloc - allocates memory for an array
