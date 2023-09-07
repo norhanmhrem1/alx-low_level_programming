f0810c5cf8536f7330da8094d7f40a0f20bbfa97
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-07 10:22:36 -0700

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		ptr[i] = s1[i];
+	}
 	for (j = 0; j < n; j++)
 	{
 		ptr[i] = s2[j];
