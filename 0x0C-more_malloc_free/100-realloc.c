62007fa637c89223887f01ddad966dca8f098b58
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-07 17:08:45 -0700

    task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,25 +15,29 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	if (ptr == NULL)
 		return (malloc(new_size));
+
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
+	ptr1 = ptr;
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			p[i] = ptr[i];
+			p[i] = ptr1[i];
 	}
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			p[i] = ptr[i];
+			p[i] = ptr1[i];
 	}
 	free(ptr);
 	return (p);
