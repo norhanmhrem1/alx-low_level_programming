9bc40a2869eec4588e533fd1a3bc05551cd9ee1a
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-14 16:19:28 -0700

    task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,16 +10,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
 	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
 
-		if (index != (n - 1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
-
-		printf("\n");
 	}
+	printf("\n");
 	va_end(numbers);
 }
