ea9b6000d48a33e6db88dd3e048a58d876874bea
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-25 23:09:15 -0700

    task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,9 +9,9 @@ void free_listint(listint_t *head)
 	listint_t *current;
 
 	while (head)
-		{
-			current = head->next;
-			free(head);
-			head = current;
-		}
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
 }
