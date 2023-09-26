8686296ca886626008e5025a77118d6459d8422e
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-25 22:10:02 -0700

    task

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,8 +15,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new_node == NULL || head == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = *head;
+	(*new_node).n = n;
+	(*new_node).next = *head;
 	*head = new_node
 
 	return (new_node);
