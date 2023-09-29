ab328ef8828f6284e300fc3ffd4f0cc380013dd0
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-28 19:43:33 -0700

    task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > sizeof(n) * 8)
 		return (-1);
 
-	*n = (~(1UL << index) & *n);
+	*n &= ~(1UL << index);
 	return (1);
 }
