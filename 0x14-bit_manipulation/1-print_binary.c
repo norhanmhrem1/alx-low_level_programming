6bf7a7fa570811e60e607e48e3cf2f673137f988
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-28 17:01:24 -0700

    task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,13 @@
 void print_binary(unsigned long int n)
 {
 	int i, count = 0;
-	unsigned long int current;
+	unsigned long int num;
 
-	for (i = 31; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
-		current = n >> i;
+		num = n >> i;
 
-		if (current & 1)
+		if (num & 1)
 		{
 			_putchar('1');
 			count++;
