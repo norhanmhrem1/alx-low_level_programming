ab254bb50fc5e7fd63481f414f226736abe40fa8
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-28 19:17:33 -0700

    task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,10 +11,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_val;
 
-	if (index > 63)
+	if (index > sizeof(n) * 8)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
+	return ((n >> index) & 1);
 
 	return (bit_val);
 }
