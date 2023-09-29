69c0c7e77728d3444b0ef901cf59b891ce8d636f
noormhrem1 <norhan_mehrem@yahoo.com>
2023-09-28 19:10:06 -0700

    task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,25 +3,23 @@
 /**
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
-*/
+ */
 
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int num;
+int bits = sizeof(n) * 8;
+int count = 0;
 
-	for (i = 63; i >= 0; i--)
-	{
-		num = n >> i;
-
-		if (num & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
-		_putchar('0');
+while (bits)
+{
+if (n & 1LU << --bits)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0');
 }
